a2f615fb55c25ce545acbbe043ba350a0f39c527
Ikram Boublaou <102217342+boublaou@users.noreply.github.com>
2022-11-30 11:56:30 -0300

    Add files via upload

---
+++ Conta.cs
@@ -0,0 +1,40 @@
+public class Conta
+{
+    public int Codigo { get; }
+    public double Saldo { get; private set; }
+
+    public Conta(int codigo)
+    {
+        Codigo = codigo;
+        Saldo = 0.0;
+    }
+
+    public void Sacar(double valor)
+    {
+        if(valor > Saldo)
+        {
+            throw new ArgumentException("Não há saldo suficiente para esse saque");
+        }
+
+        ValidarValor(valor);
+        Saldo = Saldo - valor;
+    }
+
+    public void Depositar(double valor)
+    {
+        ValidarValor(valor);
+        Saldo = Saldo + valor;
+    }
+
+    public void Transferir(double valor, Conta conta)
+    {
+        Sacar(valor);
+        conta.Depositar(valor);
+    }
+
+    private void ValidarValor(double valor)
+    {
+        if (valor <= 0.0)
+        throw new ArgumentException("O valor tem que ser maior que zero");
+    }
+}
